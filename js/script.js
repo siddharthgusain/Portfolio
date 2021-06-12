7ae690b3b80bb587e99acbfc425883ea474a21a1
siddharth <siddharthgusain26011999@gmail.com>
2021-06-12 16:30:29 +0530

    added popup

--- js/script.js
+++ js/script.js
@@ -12,3 +12,37 @@ tabsContainer.addEventListener("click",(e) => {
         aboutSection.querySelector(target).classList.add("active");
     }
 });
+
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src = 
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
+
+document.addEventListener("click" , (e) =>{
+    if(e.target.classList.contains("pp-inner")){
+        togglePortfolioPopup();
+    }
+});
+
+document.addEventListener("click" , (e) =>{
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
+
+document.querySelector(".pp-close").addEventListener("click" , togglePortfolioPopup);
+
